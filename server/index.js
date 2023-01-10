e385dbbb19c5c2ee9cbafbad861af63e5fa21aa9
Arya Karimi <devaryakrm@gmail.com>
2023-01-10 23:17:19 +0300

    Models Added

--- server/index.js
+++ server/index.js
@@ -6,6 +6,18 @@ dotenv.config();
 const port = process.env.PORT;
 const app = express();
 
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
+
 app.listen(port, () => {
   console.log(`The server is listening on http://localhost:${port}`);
+  connect();
 });
---
+++ server/models/Video.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    imgUrl: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: Number,
+      required: true,
+    },
+    likes: {
+      type: [String],
+      default: [],
+    },
+    disLikes: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Video", VideoSchema);
